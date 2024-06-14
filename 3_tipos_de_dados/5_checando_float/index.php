7c9138e4d3f445f8932bc2d5061bdffdb533cb6d
David Monteiro <dmonn97@gmail.com>
2024-06-13 21:28:56 -0300

    Update index.php

--- 3_tipos_de_dados/5_checando_float/index.php
+++ 3_tipos_de_dados/5_checando_float/index.php
@@ -1,7 +1,21 @@
 <?php
 
-    if(is_float(1.5)) {
-        echo "É Float";
+    $a = "Teste";
+    $b = 12.8;
+
+    if(is_float($a)) {
+        echo "Não é float 1! <br>";
     }
 
+    if(is_float($b)) {
+        echo "É Float 2! <br>";
+    }
+
+    if(is_float(6565.63)) {
+        echo "É um Float 3! <br>";
+    }
+
+    if(is_float("Teste")) {
+        echo "Não é um Float 4!";
+    }
 ?>
