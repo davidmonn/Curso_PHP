1bf661f493d21c93f38f2f23d56452c7addc5a37
David Monteiro <dmonn97@gmail.com>
2024-06-14 22:50:12 -0300

    Create index.php

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "Testando textos de aspas duplas <br>";
+    echo "Ele disse 'Ola!'<br>";
+    echo 'Ele disse "Ola!" <br>';
+
+    $idade = 15;
+    echo "<br>";
+    echo "Ele tem $idade anos <br>";
+    echo 'Ele tem $idade anos <br>';
+
+
+?>
