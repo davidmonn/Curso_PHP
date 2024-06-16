dd9fbf7056385c03bde42e4e873508282cca7291
David Monteiro <dmonn97@gmail.com>
2024-06-16 16:11:29 -0300

    Update index.php

--- 3_tipos_de_dados/12_arrays/index.php
+++ 3_tipos_de_dados/12_arrays/index.php
@@ -1,5 +1,27 @@
 <?php
 
-    
+    $a = [1, 2, 3]; // Todos separados por virgulas
+
+    print_r($a);
+
+    echo "<br>";
+    echo "<br>";
+
+    echo $a[1];
+
+    echo "<br>";
+    echo "<br>";
+
+    $arr = ["David", 1, 2, "Tita", 3, 4, true];
+
+    print_r($arr);
+    echo "<br>";
+    echo "<br>";
+
+    print_r($arr[3]);
+    echo "<br>";
+    echo "<br>";
+
+    print_r($arr[6]);
 
 ?>
