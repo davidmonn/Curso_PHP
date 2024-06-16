91feb6e3984e8fa81c13f1feadcb52df25a279bb
David Monteiro <dmonn97@gmail.com>
2024-06-16 14:10:18 -0300

    Create index.php

---
+++ 3_tipos_de_dados/10_booleanos/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo true;
+    echo "<br>";
+
+    echo false;
+
+    if(true)  { // True - Verdadeiro
+        echo "É verdadeiro";
+    }
+
+    if(5 > 2) { // Verdadeiro
+        echo "5 é > 2";
+    }
+
+    $podeEntrar = true;
+    
+    if($podeEntrar) {
+        echo "O usuario pode entrar";
+    }
+?>
