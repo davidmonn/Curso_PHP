cf5d95fa19d92b78754d4d27da903dacc2075157
David Monteiro <dmonn97@gmail.com>
2024-06-16 00:30:36 -0300

    up

---
+++ 3_tipos_de_dados/8_checando_strings/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $n1 = "David";
+    $n2 = 21;
+
+    if(is_string($n1)) {
+        echo "True - $n1 é uma String <br>";
+    }
+
+    if(is_string($n2)) {
+        echo "Não <br>";
+    }
+
+    else{
+        echo "Não é uma String <br>";
+    }
+
+?>
