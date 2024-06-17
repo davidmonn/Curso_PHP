63a23cb803d188d19ee1a4ff23bbed59d35fe859
David Monteiro <dmonn97@gmail.com>
2024-06-17 20:04:32 -0300

    tipos de dados

---
+++ 3_tipos_de_dados/17_null/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo NULL;
+
+    $nome = NULL;
+
+    if(is_null($nome)) {
+        echo "O valor é nulo! <br>";
+    }
+
+    $nome = "David";
+
+    if(is_null($nome)) {
+        echo "$nome <br>";
+    }
+
+?>
