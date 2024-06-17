a92faf414c7c6d05011c5faf355b23a87e09eca1
David Monteiro <dmonn97@gmail.com>
2024-06-17 20:45:38 -0300

    ex08

---
+++ 4_variaveis/2_ex08/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nome = "David"; //String
+    $numI = 27; // Inteiro
+    $numB = 1.77; //Booleano
+
+    $itens = ["Violao", "Notebook","Celular", "Fone de Ouvido"]; // Array
+
+    //print_r("Nome: $nome idade: $numI altura: $numB");
+    echo "Nome: $nome idade: $numI altura: $numB <br>";
+
+    print_r($itens);
+
+
+?>
