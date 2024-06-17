7836ba914b58aee93fe1f690515090fe65b1cfb4
David Monteiro <dmonn97@gmail.com>
2024-06-17 20:29:45 -0300

    criacao de variaveis

---
+++ 4_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $teste = "Teste 1";
+    echo "$teste <br>";
+
+    $num = 10;
+    echo "$num <br>";
+
+    $_nome = "David";
+    echo "$_nome 'Podemos usar o underline ( _ ) no PHP para uma variavel'<br>";
+
+?>
