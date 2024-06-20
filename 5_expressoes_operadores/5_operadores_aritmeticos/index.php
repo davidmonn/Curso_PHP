2235f2a03235c38ab28c2b25c53af4bc8ab3e859
David Monteiro <dmonn97@gmail.com>
2024-06-20 20:53:59 -0300

    up

---
+++ 5_expressoes_operadores/5_operadores_aritmeticos/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $soma = 5 + 5;
+    $subtracao = 5 - 2;
+    $multiplicacao = 5 * 5;
+    $divisao = 5 / 2;
+
+    echo "5 + 5 = " . $soma . " Soma <br>";
+    echo "5 - 2 = " . $subtracao . " Subtração <br>";
+    echo "5 * 5 = " . $multiplicacao . " Multiplicação <br>";
+    echo " 5 / 2 = " . $divisao . " divisão <br>";
+
+?>
