5b2fbaa41ccd94721eb77d75245455909e935146
David Monteiro <dmonn97@gmail.com>
2024-06-19 21:49:11 -0300

    Escopo dos Parametros

---
+++ 4_variaveis/9_escopo_dos_parametros/index.php
@@ -0,0 +1,15 @@
+<?php
+
+     function soma($a, $b) {
+
+        echo "$a + $b = "; // Fiz dessa forma para ela ficar como uma string e mostrar os valores da soma com os ""
+        echo $a + $b; // Sem as aspas "", ela ja ira fazer a soma
+        echo "<br>";
+
+     }
+
+     soma(1, 2);
+     soma(3, 4);
+     soma(5, 10);
+
+?>
