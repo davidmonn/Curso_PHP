49cdc2baaa390c8a3cca3d243552f38d165a6640
David Monteiro <dmonn97@gmail.com>
2024-06-22 22:39:00 -0300

    comparacao

---
+++ 5_expressoes_operadores/13_operadores_d_comparacao/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $str = "5";
+    $str2 = "2";
+    $inteiro = 5;
+
+    if($str === $inteiro) {
+        echo "Identico";
+    }
+
+    else{
+        echo "Não sao identicos <br> <br>";
+    }
+
+    if($str === $str2){
+        echo "São identicos";
+    }
+
+    else{
+        echo "Sáo Diferentes, ou valor ou tipo";
+    }
+
+?>
