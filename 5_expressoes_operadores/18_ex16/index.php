7b5a4a92e0320350d74790c1b5e1b023849c0719
David Monteiro <dmonn97@gmail.com>
2024-06-24 17:45:31 -0300

    Exercicio 16

---
+++ 5_expressoes_operadores/18_ex16/index.php
@@ -0,0 +1,34 @@
+<?php
+
+    $a = 5;
+    $b = 3;
+
+    //Operador de Igualdade
+    if($a == $b) {
+        echo "A é igual a B - 1<br>";
+    }
+
+    else{
+        echo "A é diferente de B - 1-2<br>";
+    }
+
+    // Operador de Diferenca
+    if($a != $b) {
+        echo "A é diferente de B - 2 <br>";
+    }
+
+    // Operador de Identico a
+    if($a === $b) {
+        echo "A é identico a B 3<br>";
+    }
+
+    else {
+        echo "A não é identico a B 3-2<br>";
+    }
+
+    // Operador não Identico a
+    if($a !== $b) {
+        echo "A não é identico a B 4<br>";
+    }
+
+?>
