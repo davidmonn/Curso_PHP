f9d1655134b57201d22332c8aa90ccad8fe5a3d5
David Monteiro <dmonn97@gmail.com>
2024-06-24 16:55:41 -0300

    operador de diferenca

---
+++ 5_expressoes_operadores/15_operador_identico_a/16_operador_d_diferenca/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 3;
+$b = 4;
+
+if($a != $b) {
+    echo "Testando diferenca 1 <br>";
+}
+
+if($a != 3) {
+    echo "Testando diferenca 2 <br>";
+}
+
+if(false != "teste") {
+    echo "Testando diferenca 3 <br>";
+}
+
+?>
