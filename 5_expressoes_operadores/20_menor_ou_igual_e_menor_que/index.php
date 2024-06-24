e5cb9b13dc94c8b08ddaed5e9f79e2142070e666
David Monteiro <dmonn97@gmail.com>
2024-06-24 20:12:51 -0300

    menor ou igual e menor que

---
+++ 5_expressoes_operadores/20_menor_ou_igual_e_menor_que/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    if(4 < 12) {
+        echo "4 é menor que 12 - 1 (1) <br>";
+    }
+
+    if(24 < 12) {
+        echo "24 é menor que 12 - 1 (2) <br>";
+    }
+
+    if(12 <= 12) {
+        echo "12 é menor ou igual a 12 - 2 (1) <br>";
+    }
+
+    $a = 5;
+    $b = 18;
+
+    if($a <= $b) {
+        echo "A é menor ou igual a B - 3 (1) <br>";
+    }
+
+    if($b <= $a) {
+        echo "B é menor ou igual a A - 3 (2) <br>";
+    }
+
+    if($a <= 42) {
+        echo "A é menor ou igual a 42 - 4 (1) <br>";
+    }
+
+?>
