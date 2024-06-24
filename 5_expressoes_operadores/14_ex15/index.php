91514cf25fd6904bc039c75e8a130b51a7e4b8fd
David Monteiro <dmonn97@gmail.com>
2024-06-23 22:10:02 -0300

    ex 14

---
+++ 5_expressoes_operadores/14_ex15/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    $a = 2;
+    $b = 2;
+    $c = "David";
+    $d = "David";
+    
+    if($a == $c) {
+        echo "Coincide 1<br>";
+    }
+
+    else{
+        echo "São diferentes 2<br>";
+    }
+
+    if($a == $b){
+        echo "Coincide 3";
+    }
+
+?>
