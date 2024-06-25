c881feb6b90af961bb7e98881fa1e46a75cfe3c7
David Monteiro <dmonn97@gmail.com>
2024-06-25 20:31:10 -0300

    Operador ternario

---
+++ 5_expressoes_operadores/29_operador_ternario/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    // True
+    echo 20 > 10 ? "True = 20 > 10 <br>" : "Deu false <br>";
+
+    //False
+    echo 20 < 10 ? "True" : "False = 20 < 10 <br>";
+
+    $a = 10;
+    $b = 5;
+
+    //True
+    echo $a >= $b ? "True = 10 >= 5 <br>" : "False <br>";
+
+    echo $a === $b ? "True" : "False = $a === $b <br>";
+
+    echo $a === $b && 10 > 5 ? "True" : "False = $a === $b && 10 > 5 <br>";
+
+    echo $a < $b || 5 > 4 ? "True = $a < $b || 5 > 4 <br>" : "False = $a < $b || 5 > 4 <br>";
+
+?>
