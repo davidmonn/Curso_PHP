61db13599efdae2529e48e8c5f1332989150c8c1
David Monteiro <dmonn97@gmail.com>
2024-06-25 20:02:37 -0300

    operador atribuicao

--- 5_expressoes_operadores/28_operador_atribuicao/index.php
+++ 5_expressoes_operadores/28_operador_atribuicao/index.php
@@ -1,5 +1,40 @@
 <?php
 
+   // +=
+   $a = 0;
+   $a += 10; // É a mesma coisa que $a + 10
    
+   echo $a ."<br>";
+
+   // -=
+   $b = 0;
+   $b -= 5; // $b = $b -5
+
+   echo $b . "<br>";
+
+    // *=
+    $c = 5;
+    $c *= 3; // $c = $c *3
+
+    echo $c . "<br>";
+
+   // /=
+   $d = 5;
+   $d /= 2; // $d = $d /2
+
+   echo $d . "<br>";
+
+   // /=
+   $e = 5;
+   $e %= 2; // $e = $e %2
+
+   echo $e . "<br>";
+
+   // Utilizacao com Variaveis
+   $x = 10;
+   $y = 20;
+
+   $x += $y; // $x = $x + $y
+   echo $x . "<br>";
 
 ?>
