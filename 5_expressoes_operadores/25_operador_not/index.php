d07fcc454284bd6cd858f10ea91fd9d898cd5f2c
David Monteiro <dmonn97@gmail.com>
2024-06-25 18:13:14 -0300

    Operador not

---
+++ 5_expressoes_operadores/25_operador_not/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    if(!(5 > 2)) { // True > False
+        echo "A operação 1 é True <br>";
+    }
+
+    if(!(5 > 20)) { // False > True
+        echo "A operação 2 é True <br>";
+    }
+
+    $a = 10;
+    $b = 20;
+
+    if(!($a >= $b)) {
+        echo "A operação 3 é True <br>";
+    }
+
+?>
