b9e076def72f788e7247f17afd845fab6e2ec6f0
David Monteiro <dmonn97@gmail.com>
2024-06-26 16:37:37 -0300

    else

---
+++ 6_estrutura_d_controle/4_else/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    if(5 > 2) {
+        echo "Entrou no If 1<br>";
+    } else {
+        echo " Entrou no else 1<br>";
+    }
+
+    if("teste" === 5) {
+        echo "Entrou no if 2<br>";
+    }  else {
+        echo "Entrou no else 2<br>";
+    }
+
+    $a = 10;
+    $b = 20;
+
+    if($a > $b) {
+        
+    } else {
+        echo "Entrou no else 3 <br>";
+    }
+
+?>
