16ac607788522fa461901009e33ca6367ffc9b2a
David Monteiro <dmonn97@gmail.com>
2024-06-30 16:58:46 -0300

    exercicio verificacao de categoria 4

---
+++ 6_estrutura_d_controle/16_ex_verifacao_categoria_4/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    function verificarCategoria(string $categoria) {
+        switch ($categoria) {
+            case "eletrônicos":
+                return "Essa categoria é de produtos eletrônicos";
+                
+            case "vestuário": 
+                return "Essa categoria é de produtos de vestuário";
+
+            case "alimentos":
+                return "Essa categoria é de produtos alimentícios";
+
+            default:
+                return "Categoria desconhecida";
+        }
+    }
+
+    echo verificarCategoria("eletronicos");
+    echo verificarCategoria("vestuário");
+    echo verificarCategoria("alimentos");
+    echo verificarCategoria("Desconhecida");
+    
+
+?>
