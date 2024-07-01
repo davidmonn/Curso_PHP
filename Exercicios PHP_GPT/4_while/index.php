2201577343b3a691d693633fdc2087af747c7236
David Monteiro <dmonn97@gmail.com>
2024-07-01 20:42:55 -0300

    Update index.php

--- Exercicios PHP_GPT/4_while/index.php
+++ Exercicios PHP_GPT/4_while/index.php
@@ -1,5 +1,17 @@
 <?php
 
-    
+    $dados = [10, "Maçã", 5, "Laranja", 8, "Banana", 15, "Abacaxi"];
+
+    $i = 0;
+
+    while($i < count($dados)) {
+
+        if(is_int($dados[$i])) {
+            echo $dados[$i] . "<br>";
+        }
+
+        $i++;
+
+    }
 
 ?>
