3bd5da7687cc30d66f89126852b2129df7d8f116
David Monteiro <dmonn97@gmail.com>
2024-07-08 20:29:43 -0300

    Exercicio 32 - multiplicacao - function

---
+++ 9_funcoes_php/4_ex32/index.php
@@ -0,0 +1,33 @@
+<?php
+
+    /*
+    function multi($n1, $n2, $n3) {
+        $resultado = $n1 * $n2 * $n3;
+        return $resultado;
+    }
+
+    $n1 = 5;
+    $n2 = 2;
+    $n3 = 2;
+
+    echo multi($n1, $n2, $n3);
+    */
+
+    // OU
+
+    // Definicao
+    function multi(){
+        $a = 5;
+        $b = 2;
+        $c = 2;
+
+        $resultado = $a * $b * $c;
+
+        echo $resultado;
+    }
+
+    //Invocacao
+
+    multi();
+
+?>
