45db6e6b4b6ad4c397c67dd171fade0c9455691d
David Monteiro <dmonn97@gmail.com>
2024-07-07 22:02:51 -0300

    function

---
+++ 9_funcoes_php/2_chamando_funcoes/index.php
@@ -0,0 +1,19 @@
+<?php
+
+    function soma() {
+
+        $a = 5;
+        $b = 4;
+
+        echo $a + $b . "<br>";
+    }
+
+    // Invocando funcao
+
+    soma();
+
+    // Funcao do PHP
+    echo strtoupper("Testando funcao");
+
+
+?>
