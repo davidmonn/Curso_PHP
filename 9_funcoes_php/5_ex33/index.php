8e29667e6b76f78beee3bfe0478d7d1d69746c50
David Monteiro <dmonn97@gmail.com>
2024-07-08 21:46:21 -0300

    Exercicio 33 - Function - Nome - Sobrenoe e Concatenacao

---
+++ 9_funcoes_php/5_ex33/index.php
@@ -0,0 +1,34 @@
+<?php
+
+    /*
+    function info($nome, $sobrenome){
+        $informacao = "Nome: $nome" . " Sobrenome: $sobrenome";
+        echo $informacao;
+    }
+
+    $nome = "David";
+    $sobrenome = "Monteiro";
+
+    info($nome, $sobrenome);
+    */
+    /*
+    function info($nome, $sobrenome){
+        $informacao = "Nome: $nome" . " Sobrenome: $sobrenome";
+
+        $nome = "David";
+        $sobrenome = "Monteiro";
+        echo $informacao;
+    }
+
+    info("David", "Monteiro");*/
+
+    function info(){
+        $nome = "David";
+        $sobrenome = "Monteiro";
+
+        echo $nome ." " . $sobrenome;
+    }
+
+    info();
+
+?>
