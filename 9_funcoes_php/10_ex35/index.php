b7f43ffc803a1d844710a9a9524d4fcaf90f9d1a
David Monteiro <dmonn97@gmail.com>
2024-07-09 20:57:24 -0300

    Exercicio 35
    Return valor ao quadrado

---
+++ 9_funcoes_php/10_ex35/index.php
@@ -0,0 +1,23 @@
+<?php
+
+    /*
+    function quadrado($n1){
+        return $n1 ** 2;
+    }
+    */
+
+    // OU
+
+    function quadrado($n1){
+        $resultado = $n1 ** 2;
+        return $resultado;
+    }
+
+    echo quadrado(5) . "<br>";
+
+    $x = quadrado(2);
+
+    echo $x . "<br>";
+    
+
+?>
