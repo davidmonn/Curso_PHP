a8c83303935fa376f059d33919ea9eaa31585d19
David Monteiro <dmonn97@gmail.com>
2024-07-16 19:47:50 -0300

    treino

--- Exercicios PHP_GPT/61_function_repeticao/index.php
+++ Exercicios PHP_GPT/61_function_repeticao/index.php
@@ -1,5 +1,36 @@
 <?php
 
-    
+    /*
+    Exercício 36
+
+Crie uma função que receba um array de números;
+Crie apenas um novo array com apenas os números que são maiores que 7;
+Retorne este novo array é imprima na tela;
+
+    */
+
+    function maioresS($valArr, $nFunc) {
+        $resultadoF = [];
+
+        for($iFunc = 0; $iFunc < count($valArr); $iFunc++) {
+            if($valArr[$iFunc] > $nFunc) {
+                array_push($resultadoF, $valArr[$iFunc]);
+            }
+        }
+        return $resultadoF;
+    }
+
+    $numbers = [];
+    $escolha = 7;
+
+    foreach(range(8,30) as $ind) {
+        if($ind <= 30) {
+            array_push($numbers, $ind);
+        }
+    }
+
+    $resultado = maioresS($numbers, $escolha);
+
+    print_r($resultado);
 
 ?>
