477aa6333ae5e20551a1165369267bd12d33156a
David Monteiro <dmonn97@gmail.com>
2024-07-18 18:39:07 -0300

    auxiliares function

---
+++ 9_funcoes_php/16_auxiliares_funcoes/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    function soma($a, $b) {
+
+        print_r(func_get_args()); // Mostrou os valores dos argumentos
+        echo "<br>";
+        echo (func_num_args()). "<br>"; // Mostrou a quantidade de argumentos
+
+        return $a + $b;
+
+    }
+
+    soma(2,4)
+
+
+
+
+?>
