c656a2b997955a800dfbf029f82b4f1d7451cc5d
David Monteiro <dmonn97@gmail.com>
2024-07-19 20:34:34 -0300

    Treino

---
+++ Exercicios PHP_GPT/71_switch/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    /*
+    Exercício: Calculadora de Tipos de Imóveis
+Crie uma função chamada calcularPreco que recebe dois parâmetros: o tipo de imóvel e a quantidade de metros quadrados. A função deve calcular o preço com base no tipo de imóvel e na área:
+    */
+
+    function calcImoveis($local, $metros) {
+        switch($local) {
+            case 'apartamento':
+                return "2000 por metro quadrado = " . 2000* $metros . "<br>";
+            case 'casa':
+                return "3000 por metro quadrado = " . 3000 * $metros . "<br>";
+            case 'cobertura':
+                return "5000 por metro quadrado = " . 5000 * $metros . "<br>";
+            case 'loja':
+                return "4000 por metro quadrado = ". 4000 * $metros . "<br>";
+            default:
+                return "Tipo de imovel invalido <br>";
+        }
+    }
+
+    echo "Seja bem vindo, escolha uma das opções: apartamento, casa, cobertura, loja. <br>";
+
+    echo calcImoveis('apartamento', 1);
+    echo calcImoveis('casa', 3);
+    echo calcImoveis('cobertura', 2);
+    echo calcImoveis('loja', 5);
+
+?>
