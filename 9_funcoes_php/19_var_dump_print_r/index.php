10734b72f8f24563cbb5c21211a56940231a2a70
David Monteiro <dmonn97@gmail.com>
2024-07-21 19:27:28 -0300

    var_dump e print_r

---
+++ 9_funcoes_php/19_var_dump_print_r/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $arr = [
+        'Teste',
+        123,
+        12.23,
+        true,
+        [1,2,3]
+    ];
+
+    print_r($arr);
+
+    echo "<br>";
+    echo "<br>";
+    
+    var_dump($arr);
+
+?>
