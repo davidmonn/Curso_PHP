7dfef1d4f430154c8944e2001e572139cd0dd77a
David Monteiro <dmonn97@gmail.com>
2024-07-21 21:38:25 -0300

    Create index.php

---
+++ 10_avancando_em_strings/1_interpolacao/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // O ideal e se manter apenas com uma das sintaxes.
+
+    $nome = "David";
+    $idade = 27;
+    $profissao = "programador";
+
+    echo "Eu sou $nome tenho $idade anos, e pretendo atuar como $profissao<br>";
+
+    echo "Eu sou {$nome} tenho {$idade} anos, e pretendo atuar como {$profissao} <br>";
+
+?>
