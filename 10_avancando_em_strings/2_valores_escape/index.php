1f9373e98cc0519071c455ce744c863f92af144b
David Monteiro <dmonn97@gmail.com>
2024-07-21 22:43:13 -0300

    valores de escape

---
+++ 10_avancando_em_strings/2_valores_escape/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    header("content-type: text/plain"); // Faz com que o conteudo desse arquivo de php seja texto puro
+
+    // Pular linha
+    echo "Isso ficara na primeira linha \n e isso na segunda linha \n";
+
+    // Tab
+    echo "Teste de \tab tab \n";
+
+    // Barra invertida
+
+    echo "Teste de \\ barra invertida \n";
+
+    // Dolar
+
+    echo "Teste para imprimir \$ dolar \n";
+
+    // Nao e muito relevante para web, mas para sistemas php que lidam com textos.
+
+?>
