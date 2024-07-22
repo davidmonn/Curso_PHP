7ef254297b6f0f7eb002cdcbbb212685ab213c9d
David Monteiro <dmonn97@gmail.com>
2024-07-22 20:55:06 -0300

    funcao printf

---
+++ 10_avancando_em_strings/5_funcao_printf/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $nome = "David";
+
+    // %s -> string
+    printf("O nome é %s <br> ", $nome);
+
+    // %d -> inteiro
+
+    $n = 27;
+    printf("O numero é %d e o outro numero é %d <br>", $n, 150);
+
+    //%f -> float
+
+    $float = 15.55;
+    printf("O numero float é %.2f e o outro é %f <br>", $float, 12.55); // %. serve para delimitar o tanto de casas apos a virgula no caso usamos 2.
+
+?>
