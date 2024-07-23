3af530d9981dd4c9ebda3d019fdc030e65bccdbb
David Monteiro <dmonn97@gmail.com>
2024-07-23 16:24:00 -0300

    limpando strings

---
+++ 10_avancando_em_strings/9_limpando_strings/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $str1 = "    David    ";
+
+    echo "Esta e a string 1: $str1. <br>";
+
+    $limpStr1 = trim($str1);
+
+    echo "Esta e a string 1: $limpStr1. <br>";
+
+    $strLimpa2 = rtrim($str1);
+
+    echo "Esta e a string 1: $strLimpa2. <br>";
+
+?>
