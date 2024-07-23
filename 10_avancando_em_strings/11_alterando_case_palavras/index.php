b79d24571a2d2ee16d3f4054a50ce3a5b18a8c33
David Monteiro <dmonn97@gmail.com>
2024-07-23 19:26:24 -0300

    alterando case das palavras

---
+++ 10_avancando_em_strings/11_alterando_case_palavras/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $nomeComp = "david monteiro matias adao <br>";
+
+    // Todas as iniciais maisculas
+    echo ucwords($nomeComp);
+
+    $text = "aqui temos o uso de ucfirst, para deixar a primeira letra maiscula <br>";
+
+    // Primeira letra em maisculo
+    echo ucfirst($text);
+
+?>
