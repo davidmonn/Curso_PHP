4b0c8d4d77ed78b1143403afce76aa9df3dfbb81
David Monteiro <dmonn97@gmail.com>
2024-07-23 19:40:23 -0300

    exericio 41 - 2

---
+++ 10_avancando_em_strings/12_ex41_2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo ucfirst("este item esta em "), strtoupper("promoção");
+
+    echo "<br>";
+    echo "<br>";
+
+    $frase = "este item esta em ";
+    $promo = "promoção";
+
+    echo ucfirst($frase) . strtoupper($promo);
+
+?>
