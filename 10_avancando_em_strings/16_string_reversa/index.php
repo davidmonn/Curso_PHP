0d6fd36a6d2fe81ede31b72df9e710f396d00050
David Monteiro <dmonn97@gmail.com>
2024-07-24 16:35:45 -0300

    strrev

---
+++ 10_avancando_em_strings/16_string_reversa/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $palavra = "Testando";
+    $palavraInvertida = strrev($palavra);
+    
+    echo $palavra . "<br>";
+    echo $palavraInvertida . "<br>";
+
+    $frase = "o programador estava com o prazo curto para fazer o sistema";
+    $fraseInvertida = strrev($frase);
+
+    echo ucfirst($frase) . "<br>";
+    echo ucfirst($fraseInvertida) . "<br>";
+
+    
+?>
