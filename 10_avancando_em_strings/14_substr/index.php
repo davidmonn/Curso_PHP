30eee7abc149b5a7f5d27a33fe277983606873e6
David Monteiro <dmonn97@gmail.com>
2024-07-23 21:53:28 -0300

    ATT

--- 10_avancando_em_strings/14_substr/index.php
+++ 10_avancando_em_strings/14_substr/index.php
@@ -2,10 +2,19 @@
 
     $str = "Esta é a minha string";
 
-    $minha = substr($str, 10, 5);
+    $minha = substr($str, 10, 5); // String pai, INDICE INICIAL, COMPRIMENTO DA PALAVRA.
 
     echo $str . "<br>";
 
     echo $minha . "<br>";
+
+    $str2 = "Testando esta string abc";
+
+    $novaPalavra = substr($str2, 9); // OMITIR COMPRIMENTO = PEGA ATE O FIM
+
+    echo $novaPalavra;
+
+    $novaPalavra2 = substr($str2, 10, -3); // COMPRIMENTO NEGATIVO = REMOVE DO ULTIMO INDICE
+    echo $novaPalavra2; // eliminou o abc com o -3
 
 ?>
