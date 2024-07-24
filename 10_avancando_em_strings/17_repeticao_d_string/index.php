db6bc48898d7f38f33de01f4086b6d6fbe94c056
David Monteiro <dmonn97@gmail.com>
2024-07-24 19:22:18 -0300

    str_repeat

---
+++ 10_avancando_em_strings/17_repeticao_d_string/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $repeat = "Testando";
+
+    echo str_repeat($repeat, 4) . "<br>";
+
+    $frase = "testando repetição por frase. ";
+
+    $repeatFrase = str_repeat($frase, 3);
+
+    echo ucfirst($repeatFrase) . "<br>";
+
+?>
