b363826f8f1e829e0a140675fbf1b4888f13e10a
David Monteiro <dmonn97@gmail.com>
2024-07-25 22:18:04 -0300

    Range
    Usado para criar um array numerico rapidamente

---
+++ 11_avancando_em_arrays/3_range_criar_array_rapidamente/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $n = range(1, 15);
+    print_r($n);
+    echo "<br>";
+    echo "<br>";
+
+    $n2 = range(5, 50);
+    print_r($n2);
+    echo "<br>";
+    echo "<br>";
+
+    $n3 = range(0, 500, 100); // Dessa forma foi adicionado um step de 100, ou seja 0, 100, 200 ,300... 500
+    print_r($n3);
+    echo "<br>";
+
+?>
