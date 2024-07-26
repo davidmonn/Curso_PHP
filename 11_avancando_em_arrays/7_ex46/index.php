c843ec8ab25b7799ab81ad84bfaf1aebc02cb4a1
David Monteiro <dmonn97@gmail.com>
2024-07-26 20:59:46 -0300

    exercicio 46
    Feito de forma simples apenas imprimindo com echo

--- 11_avancando_em_arrays/7_ex46/index.php
+++ 11_avancando_em_arrays/7_ex46/index.php
@@ -1,5 +1,44 @@
 <?php
 
-    
+    /*
+    Exercício 46
+Crie um array multidimensional com 3 arrays que tem 4 elementos cada;
+Imprima todos os elementos de cada um dos arrays;
+Imprima também quando está mudando de array;
+    */
+
+    $multArr = [
+        [
+        'nome' => 'David',
+        'idade' => 27,
+        'hobbie' => 'violao',
+        'profissao' => 'programador'
+    ],
+    [
+        'nome' => 'Bianca',
+        'idade' => 21,
+        'hobbie' => 'Sair',
+        'profissao' => '...'
+    ],
+    [
+        'nome' => 'Suzana',
+        'idade' => 24,
+        'hobbie' => 'Guitarra',
+        'profissao' => 'programadora'
+    ]
+    ];
+
+    echo $multArr[0]['nome'] ."<br>";
+    echo $multArr[0]['idade'] . "<br>";
+    echo $multArr[0]['hobbie'] . "<br>";
+    echo $multArr[0]['profissao'] . "<br>";
+    echo "<br>";
+
+    echo $multArr[1]['nome'] . "<br>";
+    echo $multArr[1]['profissao'] . "<br>";
+    echo "<br>";
+
+    echo $multArr[2]['nome'] . "<br>";
+    echo $multArr[2]['hobbie'] . "<br>"
 
 ?>
