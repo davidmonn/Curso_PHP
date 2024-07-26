5a933331b9b020dbf10cbb572363d9199f66bbab
David Monteiro <dmonn97@gmail.com>
2024-07-26 16:56:21 -0300

    count

---
+++ 11_avancando_em_arrays/11_numero_de_elementos/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $arr = [];
+    echo count($arr) . "<br>"; 
+
+    $arr2 =[1,2,3];
+    echo count($arr2) . "<br>";
+
+    $arr3 = range(1,10);
+    echo count($arr3) . "<br>";
+
+    $arrAssoc = ['nome' => 'David', 'idade' => 27, 'profissao' => 'programador'];
+    echo count($arrAssoc) . "<br>";
+
+?>
