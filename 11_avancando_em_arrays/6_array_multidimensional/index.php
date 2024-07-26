a9c5bb40f0e2d77d6c53da4b6b3dfba122690d2b
David Monteiro <dmonn97@gmail.com>
2024-07-26 20:24:27 -0300

    Array dimensional

---
+++ 11_avancando_em_arrays/6_array_multidimensional/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $arrM = [
+        [1,2,3],
+        [2,4,6]
+    ];
+
+    print_r($arrM);
+    echo "<br>";
+
+    echo $arrM[0][1] . " Acessa segundo elemento do primeiro array <br>"; // Acessando primeiro array e segundo elemento
+
+    echo $arrM[1][2] . " acessa ultimo elemento, segundo array <br>";  // Acessando segundo array e ultimo elemento
+
+    echo count($arrM) ." Elementos array princial <br>"; // Mostra a quantidade de elementos no array principal
+    echo count($arrM[0]) ." Quantidade do primeiro array <br>"; // Mostra o count do primeiro array
+
+?>
