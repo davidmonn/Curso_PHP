7b3acaf08c3fab9fc6eaea524f7b24f239553d04
David Monteiro <dmonn97@gmail.com>
2024-07-26 22:19:16 -0300

    exercicio 46
    feito com uso de if e function isset para verificar se tem uma chave existente na array com o nome selecionado

---
+++ 11_avancando_em_arrays/7_ex46_2/index.php
@@ -0,0 +1,40 @@
+<?php
+
+    function usuarios($buscar) {
+        $recbBuscador = [];
+
+        if(isset($buscar['nome'])) {
+            $recbBuscador = $buscar;
+        }
+        return $recbBuscador;
+    }
+
+    $multArr = [
+        [
+        'nome' => 'David',
+        'idade' => 27,
+        'hobbie' => 'violao',
+        'profissao' => 'programador'
+    ],
+    [
+        'nome' => 'Bianca',
+        'idade' => 21,
+        'hobbie' => 'Sair',
+        'profissao' => '...'
+    ],
+    [
+        'nome' => 'Suzana',
+        'idade' => 24,
+        'hobbie' => 'Guitarra',
+        'profissao' => 'programadora'
+    ]
+    ];
+
+    $result = usuarios($multArr[0]);
+    print_r($result);
+    echo "<br>";
+
+    $result2 = usuarios($multArr[1]);
+    print_r($result2);
+
+?>
