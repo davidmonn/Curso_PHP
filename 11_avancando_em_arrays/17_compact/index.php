27eda5fff402588b59b9cca982d414b3030785ab
David Monteiro <dmonn97@gmail.com>
2024-07-29 20:44:53 -0300

    compact

---
+++ 11_avancando_em_arrays/17_compact/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    $modelo = "mecanico";
+    $cor = "branco";
+    $luzes = "sim";
+
+    $teclado = compact('modelo', 'cor', 'luzes'); // Variavel criada para armazenar os valores como array
+
+    print_r($teclado);
+    echo "<br>";
+
+?>
