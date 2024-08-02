37091eb1dc8a236c7a4c38edd611721fe4c73710
David Monteiro <dmonn97@gmail.com>
2024-08-02 16:49:27 -0300

    array_reverse

---
+++ 11_avancando_em_arrays/26_array_reverse/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    $arr = [1,2,3,4,5];
+
+    $arrRev = array_reverse($arr);
+
+    print_r($arr);
+    echo "<br>";
+
+    print_r($arrRev);
+    echo "<br>";
+
+    $arr2 = ['David', 27, true, [1, 2]];
+    $arrRev2 = array_reverse($arr2);
+
+    print_r($arr2);
+    echo "<br>";
+
+    print_r($arrRev2);
+    echo "<br>";
+
+?>
