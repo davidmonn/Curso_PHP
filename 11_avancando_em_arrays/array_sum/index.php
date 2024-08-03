48f588f998781c8f751a451dd7a75f07ea8b03f1
David Monteiro <dmonn97@gmail.com>
2024-08-03 18:56:41 -0300

    array_sum
    Soma dos numeros da array

---
+++ 11_avancando_em_arrays/array_sum/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $arr = [1,5,6,8,9,55,44,63,67];
+
+    $soma = array_sum($arr);
+    print_r($soma);
+    echo "<br>";
+
+    print_r($arr); // Não altera o valor da array principal.
+
+?>
