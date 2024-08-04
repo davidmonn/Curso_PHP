500c670dfcec96e70a395963746ac235f103b582
David Monteiro <dmonn97@gmail.com>
2024-08-04 20:26:42 -0300

    Exercicio 52

---
+++ 12_introducao_orientacao_a_objetos/3_exercicio_52/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    /*
+    Exercício 52
+    Crie uma class car;
+    Instancie 3 objetos com esta classe;
+    */
+
+    class Car {
+
+    }
+
+    $fusca = new Car;
+    $wolskisvagem = new Car;
+    $fiesta = new Car;
+
+?>
