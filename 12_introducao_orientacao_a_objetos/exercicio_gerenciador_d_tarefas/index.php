3d66f8bc7cfe051f9d6d49680f6637c2ed47abab
David Monteiro <dmonn97@gmail.com>
2024-08-12 19:32:35 -0300

    Exercicio Tesk: tarefas

---
+++ 12_introducao_orientacao_a_objetos/exercicio_gerenciador_d_tarefas/index.php
@@ -0,0 +1,67 @@
+<?php
+
+    /*
+    Crie uma classe chamada Task que represente uma tarefa.
+    A classe Task deve ter os seguintes atributos:
+    title: título da tarefa.
+    description: descrição da tarefa.
+    completed: indica se a tarefa está concluída ou não.
+    A classe Task deve ter os seguintes métodos:
+    markAsCompleted(): marca a tarefa como concluída.
+    markAsIncomplete(): marca a tarefa como não concluída.
+    getTitle(): retorna o título da tarefa.
+    getDescription(): retorna a descrição da tarefa.
+    isCompleted(): retorna um booleano indicando se a tarefa está concluída ou não.
+    */
+
+    interface ConteudoTask {
+        public function markAsCompleted();
+        public function markAsIncomplete();
+        public function getTitle();
+        public function getDescription();
+        public function isCompleted();
+    }
+    
+    class Task implements ConteudoTask {
+        public $title = "Estudar";
+        public $description = "Estudar para Prova";
+        private $completed = false;
+    
+        public function markAsCompleted() {
+            $this->completed = true;
+            echo "Tarefa marcada como concluída.<br>";
+        }
+    
+        public function markAsIncomplete() {
+            $this->completed = false;
+            echo "Tarefa marcada como não concluída.<br>";
+        }
+    
+        public function getTitle() {
+            return $this->title;
+        }
+    
+        public function getDescription() {
+            return $this->description;
+        }
+    
+        public function isCompleted() {
+            return $this->completed;
+        }
+    }
+
+    $tarefa1 = new Task();
+    $tarefa1->title;
+    $tarefa1->description;
+    
+    echo "Título: " . $tarefa1->getTitle() . "<br>";
+    echo "Descrição: " . $tarefa1->getDescription() . "<br>";
+    
+    $tarefa1->markAsCompleted();
+    if ($tarefa1->isCompleted()) {
+        echo "Parabéns, você concluiu a tarefa!<br>";
+    } else {
+        echo "Tarefa incompleta.<br>";
+    }
+
+?>
