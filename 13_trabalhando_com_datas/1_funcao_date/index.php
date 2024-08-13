4a9273f573ac91ddef2de50f3acd5a2afc58e91d
David Monteiro <dmonn97@gmail.com>
2024-08-13 18:28:56 -0300

    funcao date

---
+++ 13_trabalhando_com_datas/1_funcao_date/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $d = date('d/m/y'); // day / month / year
+
+    echo $d . "<br>";
+
+    $d2 = date("D, M - Y"); // Dessa forma temos a abreviacao dos dias
+    echo $d2 . "<br>";
+
+    $d3 = date("D/M/y - w"); // Tambem podemos misturar
+    echo $d3 . "<br>";
+
+    $d4 = date("l/F/y - w");
+    echo $d4 . "<br>";
+
+?>
