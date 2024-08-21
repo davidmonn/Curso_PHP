5db8f73b117a2475c808aab21d14f526effa2d10
David Monteiro <dmonn97@gmail.com>
2024-08-21 19:02:43 -0300

    Inicio do projeto

---
+++ 15_projeto1_blog/index.php
@@ -0,0 +1,21 @@
+<?php
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- ESTILO DO PROJETO -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>Seja bem vindo</h1>
+</body>
+</html>
