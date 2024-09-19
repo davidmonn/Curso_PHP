dffbe788ae840b4a2ddc03f5af31bdc268f4f7c4
David Monteiro <dmonn97@gmail.com>
2024-09-18 23:23:19 -0300

    inicio projeto agenda

---
+++ 17_projeto_agenda/index.php
@@ -0,0 +1,26 @@
+<?php
+
+    include_once("config/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <!-- BIBLIOTECA BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap-grid.min.css" integrity="sha512-i1b/nzkVo97VN5WbEtaPebBG8REvjWeqNclJ6AItj7msdVcaveKrlIIByDpvjk5nwHjXkIqGZscVxOrTb9tsMA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT AWESOME (FONTE DE ICONES) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Projeto Agenda</title>
+</head>
+<body>
+    
+    <h1>Testando agenda</h1>
+    <i class="fas fa-eye"></i>
+
+</body>
+</html>
