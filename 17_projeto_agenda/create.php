20f4bd77489528df6f9cad92ff03b87e3946b110
David Monteiro <dmonn97@gmail.com>
2024-09-22 19:46:08 -0300

    PROJETO Agenda 06
    Feito a nossa tela de criacao de contato e algumas configuracoes de CSS

--- 17_projeto_agenda/create.php
+++ 17_projeto_agenda/create.php
@@ -4,8 +4,26 @@
 
 ?>
     
-    <h1>Adicionar contato</h1>
-    <i class="fas fa-eye"></i>
+    <div class="container">
+        <?php include_once("templates/backbtn.html"); ?>
+        <h1 id="main-title">Adicionar contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+            <input type="hidden" name="type" value="create"> <!-- Este input ira guiar o nosso projeto -->
+            <div class="form-group">
+                <label for="name">Nome do contato:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome:" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do contato:</label>
+                <input type="text" class="form-control" id="phone" placeholder="Digite o telefone:" required>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <textarea type="text" class="form-control" id="observations" placeholder="Insira as observações" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+    </div>
 
 <?php
 
