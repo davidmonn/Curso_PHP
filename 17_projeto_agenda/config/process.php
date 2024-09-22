17351e6f01f2cdc1b8b1ae88801f588f98abb3d8
David Monteiro <dmonn97@gmail.com>
2024-09-21 23:30:02 -0300

    PROJETO Agenda 5
    Feito pagina para ser visualizado contatos e suas observacoes junto das configuracoes de CSS

--- 17_projeto_agenda/config/process.php
+++ 17_projeto_agenda/config/process.php
@@ -5,7 +5,36 @@
     include_once("connection.php");
     include_once("url.php");
 
+    $id;
+
+    if(!empty($_GET)) {
+        $id = $_GET['id'];
+    }
+
+    // RETORNA O DADO DE UM CONTATO
+
+    if(!empty($id)) {
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id); // ID q veio do GET
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+        
+    } else {
+        // RETORNA TODOS OS CONTATOS
+        $contacts = [];
+
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $contacts = $stmt->fetchAll();
+    }
+
+    /* JEITO Q EU TINHA FEITO PARA TESTE
+    //RETORNA TODOS OS CONTATOS
     $contacts = [];
+
 
     // Primeira coisa que iremos fazer e puxar todos os dados da query
 
@@ -20,5 +49,5 @@
         $erro = $stmt->errorInfo();
         echo "Erro ao selecionar dados: $erro <br>";
     }
-
+    */
 ?>
