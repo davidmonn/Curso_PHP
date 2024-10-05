039b2494f04c95905065d161a058749a802abc52
David Monteiro <dmonn97@gmail.com>
2024-10-05 19:27:57 -0300

    Projeto MovieStar
    Feito as configuracoes no CSS em nossa parte de auth.php (Login/Cadastro)

--- 19_projeto_movie_star/auth.php
+++ 19_projeto_movie_star/auth.php
@@ -5,7 +5,7 @@ require_once("templates/header.php");
 <div id="main-container" class="container-fluid">
     <div class="col-md-12"> <!-- md 12, significa que sera uma div de 12 colunas ou que ela ira basicamente ocupar a tela toda  -->
         <div class="row" id="auth-row"> <!-- Aqui estamos dizendo que os itens que estao aqui dentro podem ficar um ao lado do outro, por isso nome de row (linha) -->
-            <div class="col md-4" id="login-container">
+            <div class="col-md-4" id="login-container">
                 <h2>Entrar</h2>
                 <form action="" method="POST">
                 <input type="hidden" name="type" value="login">
@@ -17,11 +17,13 @@ require_once("templates/header.php");
                         <label for="password">Senha:</label>
                         <input type="password" class="form-control" placeholder="Digite sua senha:" id="password" name="password">
                     </div>
-                    <input type="submit" class="btn card-btn" value="Entrar">
-                    <input type="reset" class="btn card-btn" value="Limpar">
+                    <div class="btn card-btn">
+                        <input type="submit" class="btn card-btn" value="Entrar">
+                        <input type="reset" class="btn card-btn" value="Limpar">
+                    </div>
                 </form>
             </div>
-            <div class="col md-4" id="register-container">
+            <div class="col-md-4" id="register-container">
                 <h2>Criar Conta</h2>
                 <form action="" method="POST">
                     <input type="hidden" name="type" value="register">
@@ -45,8 +47,10 @@ require_once("templates/header.php");
                         <label for="confirmpassword">Confirmação de senha:</label>
                         <input type="password" class="form-control" placeholder="Confirme sua senha:" id="confirmpassword" name="confirmpassword">
                     </div>
-                    <input type="submit" value="Registrar">
-                    <input type="reset" value="Limpar">
+                    <div class="btn card-btn">
+                        <input type="submit" value="Registrar" class="btn-btn">
+                        <input type="reset" value="Limpar" class="btn-btn">
+                    </div>
                 </form>
             </div>
         </div>
