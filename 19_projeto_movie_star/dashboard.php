ab38a1148e2744e6876f4d447327de1d8fc9668c
David Monteiro <dmonn97@gmail.com>
2024-10-31 19:49:48 -0300

    Projeto MovieStar
    Feito nosso dashboard, para poder ser editado a parte de nossos filmes adicionados.

---
+++ 19_projeto_movie_star/dashboard.php
@@ -0,0 +1,53 @@
+<?php
+    require_once("templates/header.php");
+
+    // Verifica se o usuario esta autenticado
+    require_once("models/User.php");
+    require_once("dao/UserDAO.php");
+  
+    $user = new User();
+    $userDao = new UserDao($conn, $BASE_URL);
+  
+    $userData = $userDao->verifyToken(true);
+?>
+
+   <div id="main-container" class="container-fluid">
+      <h2 class="section-title">Dashboard</h2>
+      <p class="section-description">Adicione ou atualize as informações de seus filmes enviados.</p>
+      <div class="col md-12" id="movies-dashboard">
+        <div class="col md-12" id="add-movie-contaier">
+            <a href="<?= $BASE_URL ?>newmovie.php" class="btn card-btn">
+                <i class="fas fa-plus"></i> Adicionar filme
+            </a>
+        </div>
+        <table class="table">
+           <thead>
+            <th scope="col">#</th>
+            <th scope="col">Título</th>
+            <th scope="col">Nota</th>
+            <th scope="col" class="actions-column">Ações</th>
+           </thead>
+           <tbody>
+              <tr>
+                <td scope="row">1</td>
+                <td><a href="#" class="table-movie-title">Título</a></td>
+                <td><i class="fas fa-star"></i>9</td>
+                <td class="actions-column">
+                    <a href="#" class="edit-btn">
+                        <i class="far fa-edit"></i>Editar
+                    </a>
+                    <form action="">
+                        <button type="submit" class="delete-btn">
+                            <i class="fas fa-times"></i>Deletar
+                        </button>
+                    </form>
+                </td>
+              </tr>
+           </tbody>
+        </table>
+      </div>
+   </div>
+
+<?php
+    require_once("templates/footer.php");
+?>
