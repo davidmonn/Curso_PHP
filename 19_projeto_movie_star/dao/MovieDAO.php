d9c09f0b082869c5735201e18954dae822c28339
David Monteiro <dmonn97@gmail.com>
2024-11-09 20:49:53 -0300

    Projeto MovieStar
    Feito nosso metodo de deletar um filme, apenas o dono do filme podera apagar o filme

--- 19_projeto_movie_star/dao/MovieDAO.php
+++ 19_projeto_movie_star/dao/MovieDAO.php
@@ -170,6 +170,13 @@
 
     public function destroy($id) {
 
+      $stmt = $this->conn->prepare("DELETE FROM movies WHERE id = :id");
+      $stmt->bindParam(":id", $id);
+      $stmt->execute();
+
+      // Mensagem de sucesso por remover um filme
+      $this->message->setMessage("Filme apagado com sucesso!", "success", "dashboard.php");
+
     }
 
   }
