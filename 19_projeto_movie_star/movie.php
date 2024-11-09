7e7b73b18a617d67cbda7af018349f1cccd4b151
David Monteiro <dmonn97@gmail.com>
2024-11-08 21:25:12 -0300

    Projeto MovieStar
    Feito parte de nosso movie adiantando algumas coisas como review e comentarios

--- 19_projeto_movie_star/movie.php
+++ 19_projeto_movie_star/movie.php
@@ -9,7 +9,6 @@
   // Pegar id do filme
 
   $id = filter_input(INPUT_GET, "id");
-  $id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
   $movie;
 
@@ -33,8 +32,20 @@
 
   }
 
+  // Checar se o filme te imagem
+  if($movie->image == "") {
+    $movie->image = "movie_cover.jpg";
+  }
 
-  
+  // Checar se o filme e do usuario
+  $userOwnsMovie = false;
+  if(!empty($userData)) {
+    if($userData->id === $movie->users_id) {
+      $userOwnsMovie = true;
+    }
+  }
+  // Resgatar as Reviews dp filme
+
 ?>
 
   <div id="main-container" class="container-fluid">
@@ -50,6 +61,61 @@
         </p>
         <iframe src="<?= $movie->trailer ?>" width="560px" height="315px" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; pictuare-in-pictuare" allowfullscreen></iframe>
         <p><?= $movie->description ?></p>
+      </div>
+      <div class="col-md-4">
+          <div class="movie-image-container" style="background-image: url('<?=$BASE_URL ?>/img/movies/<?= $movie->image ?>');"></div>
+      </div>
+      <div class="offset-md-1 col-md-10" id="reviewss-container">
+        <h3 id="reviews-title">Avaliações: </h3>
+        <!-- Verifica se habilita a review para o usuario ou nao -->
+         <div class="col-md-12" id="review-from-container">
+          <h4>Envie sua avaliação:</h4>
+          <p class="page-description">Preencha o formulário com a nota e comentário sobre o filme:</p>
+          <form action="<?= $BASE_URL ?>review_process.php" method="POST" id="review-form">
+            <input type="hidden" name="type" value="create">
+            <input type="hidden" name="movies_id" value="<?= $movie->id ?>">
+              <div class="form-group">
+                <label for="rating">Nota do filme:</label>
+                <select name="rating" id="rating" class="form-control">
+                  <option value="">Selecione
+                    <option value="10">10</option>
+                    <option value="9">9</option>
+                    <option value="8">8</option>
+                    <option value="7">7</option>
+                    <option value="6">6</option>
+                    <option value="5">5</option>
+                    <option value="4">4</option>
+                    <option value="3">3</option>
+                    <option value="2">2</option>
+                    <option value="1">1</option>
+                  </option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="review">Seu comentário:</label>
+                <textarea name="review" id="review" rows="3" class="form-control" placeholder="Digite seu comentário sobre o filme!"></textarea>
+              </div>
+              <input type="submit" value="Enviar comentário" class="btn card-btn">
+          </form>
+         </div>
+         <!-- Comentários -->
+          <div class="col-md-12 review">
+            <div class="row">
+              <div class="col-md-1">
+                <div class="profile-img-container review-image" style="background-image: url('<?=$BASE_URL ?>img/users/user.png');"></div>
+                <div class="col-md-9 author-details-container">
+                  <h4 class="author-name">
+                    <a href="<?= $movie->id ?>">David Teste</a>
+                  </h4>
+                  <p class="fas fa-star"> 9 </p>
+                </div>
+                <div class="col-md-12">
+                  <p class="comment-title">Comentário:</p>
+                  <p>Este é o comentário do usuário</p>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   </div>
