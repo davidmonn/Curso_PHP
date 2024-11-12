1c5b99fd2987707d711b550ef50a235491b6bee1
David Monteiro <dmonn97@gmail.com>
2024-11-12 16:39:24 -0300

    Projeto MovieStar
    Feito as configuracoes de perfil de um usuario.

---
+++ 19_projeto_movie_star/profile.php
@@ -0,0 +1,77 @@
+<?php
+   require_once("templates/header.php");
+
+   // Verifica se o usuario esta autenticado
+   require_once("models/User.php");
+   require_once("dao/UserDAO.php");
+   require_once("dao/MovieDAO.php");
+
+   $user = new User();
+   $userDao = new UserDAO($conn, $BASE_URL);
+   $movieDao = new MovieDAO($conn, $BASE_URL);
+
+   //Receber id do usuario
+   $id = filter_input(INPUT_GET, "id");
+
+   if(!empty($userData->id)) {
+    if($userData->id){
+
+        $id = $userData->id;
+
+    } else {
+
+        $message->setMessage("Usuário não encontrado. ", "error", "index.php");
+    }
+
+
+   } else {
+
+    $userData = $userDao->findById($id);
+
+    // Se nao encontrar o usuario
+    if($userData) {
+        $message->setMessage("Usuário não encontrado. ", "error", "index.php");
+    }
+
+   }
+
+   $fullName = $user->getFullName($userData);
+
+   if($userData->image == "") {
+     $userData->image = "user.png";
+   }
+
+   // Filmes adicionados pelo usuario
+   $userMovies = $movieDao->getMoviesByUserId($id);
+
+?>
+
+   <div id="main-container" class="container-fluid">
+      <div class="col-md-8 offset-md-2">
+        <div class="row profile-container">
+            <div class="col-md-12">
+                <h1 class="page-title"> <?= $fullName ?></h1>
+                <div id="profile-image-container" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>');" ></div>
+                <h3 class="about-title">Sobre:</h3>
+                <?php if(!empty($userData->bio)): ?>
+                    <p class="profile-description"><?= $userData->bio ?></p>
+                <?php else: ?>
+                    <p class="profile-description">O usuário ainda não escreveu nada aqui...</p>
+                <?php endif; ?>
+            </div>
+            <div class="col-md-12 added-movies-container">
+                <h3>Filmes adicionados pelo usuário:</h3>
+                <?php foreach($userMovies as $movie): ?>
+                    <?php require("templates/movie_card.php"); ?>
+                <?php endforeach; ?>
+                <?php if(count($userMovies) === 0): ?>
+                    <p class="empty-list">O usuário ainda não adicionou filmes.</p>
+                <?php endif; ?>
+            </div>
+        </div>
+      </div>
+   </div>
+
+<?php
+    require_once("templates/footer.php");
+?>
