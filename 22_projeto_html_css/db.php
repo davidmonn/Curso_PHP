ef034ecb8e6a157082eac81d30e1a70cf438a764
David Monteiro <dmonn97@gmail.com>
2024-11-29 20:55:39 -0300

    Algumas correcoes e adicioes para db

--- 22_projeto_html_css/db.php
+++ 22_projeto_html_css/db.php
@@ -1,20 +1,23 @@
 <?php
 
+
   $db_host = "localhost";
   $db_name = "testcont";
   $db_user = "root";
   $db_pass = "";
 
   try {
-
-   $conn = new PDO("mysql:db_name=" . $db_name . ";db_host=" . $db_host . $db_user . $db_pass);
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Conexão estabelecida com sucesso <br>";
-   //$conn->getAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    // Corrigindo a string de conexão
+    $conn = new PDO("mysql:host=" . $db_host . ";dbname=" . $db_name, $db_user, $db_pass);
+    
+    // Definindo o modo de erro do PDO
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Conexão estabelecida com sucesso <br>";
 
   } catch(PDOException $e) {
-    $erro = $e->getMessage();
-    echo "Falha na conexão, erro: $e";
+    // Captura o erro de conexão
+    echo "Falha na conexão, erro: " . $e->getMessage();
   }
 
 ?>
--- 22_projeto_html_css/templates/header.php
+++ 22_projeto_html_css/templates/header.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maxmum-scale=1, user-scalable=no">
     <title>Contato</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?=$BASE_URL ?>css_php/style.css">
 </head>
 <body>
     <div id="main-container">
